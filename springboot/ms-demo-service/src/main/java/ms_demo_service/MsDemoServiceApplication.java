b3bffb6e393350c66e85d73be008370e04f50b20
Akash <sureshakash081@gmail.com>
2026-07-21 13:35:21 +0530

    Add Spring Boot app and update ECS build

---
+++ springboot/ms-demo-service/src/main/java/ms_demo_service/MsDemoServiceApplication.java
@@ -0,0 +1,13 @@
+package ms_demo_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsDemoServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsDemoServiceApplication.class, args);
+	}
+
+}
---
+++ springboot/ms-demo-service/src/test/java/ms_demo_service/MsDemoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ms_demo_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsDemoServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
